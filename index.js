1edf068618530fd7bdc8754d62d0088255aa1802
eshansaif <eshansaif1234@gmail.com>
2023-12-07 17:15:14 +0600

    .env file added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("./app");
-const punycode = require("punycode/");
+require("dotenv").config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
